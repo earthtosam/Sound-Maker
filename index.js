96e20f296f4cf1e3196d06ca4ba9196d6a76bc91
Sam <samuel_ramirez_07@yahoo.com>
2019-07-12 04:09:50 -0700

    Music Maker
    Created using Javascript

---
+++ index.js
@@ -0,0 +1,34 @@
+window.addEventListener('load', () => {
+    const sounds = document.querySelectorAll(".sound");
+    const pads = document.querySelectorAll(".pads div");
+    const visual = document.querySelector(".visual");
+    const colors = [
+        "#085e86",
+        "#51eaea",
+        "#ffe285",
+        "#ff9d76",
+        "#fb3569",
+        "#881515"
+    ];
+
+    //Sound Goes Here
+    pads.forEach((pad, index) => {
+        pad.addEventListener("click", function() {
+            sounds[index].currentTime = 0;
+            sounds[index].play();
+
+            createBubbles(index);
+        });
+    });
+
+    // Function for bubbles
+    const createBubbles = (index) => {
+        const bubble = document.createElement("div");
+        visual.appendChild(bubble);
+        bubble.style.backgroundColor = colors[index];
+        bubble.style.animation = "jump 1s ease";
+        bubble.addEventListener('animationend', function(){
+            visual.removeChild(this);
+        })
+    };
+});
